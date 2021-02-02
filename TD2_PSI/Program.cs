7605ea0ac72c056a77a4749473e68c8527de19e9
Tania <taniadasilvajunior@gmail.com>
2021-02-02 12:32:59 +0100

    Ajoutez des fichiers projet.

---
+++ TD2_PSI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD2_PSI
+{
+    class MyImage
+    {
+        public MyImage(string file)
+        {
+            //lit un fichier et le transforme en instance de classe
+
+        }
+        public void From_Image_To_File(string file)
+        {
+            //prend une instance de MyImage et la transforme en fichier binaire respectant 
+            //la structure du fichier .bmp
+        }
+        public int Convertir_Endian_To_Int(byte[] tab)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
